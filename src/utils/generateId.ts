b39a50f8bbe2a2c32d42529816e31c8b782fba1c
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-09-02 10:43:33 +0900

    Design: CSS 수정 - 상대값 적용

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,6 +1,5 @@
 export const generateId = function () {
   const date = new Date().toISOString().slice(0, 10); // YYYY-MM-DD 형식의 날짜
   const randomString = Math.random().toString(36).slice(2, 11); // 무작위 문자열
-  console.log(`id-${date}-${randomString}`);
   return `id-${date}-${randomString}`;
 };
